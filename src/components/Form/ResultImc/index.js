c332d42c647dabba9a6ff8f8987bf5e3842aafaf
gabriegsti <gabrielgsdevbr@gmail.com>
2022-09-03 15:55:51 -0300

    first commit

---
+++ src/components/Form/ResultImc/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+
+export default function ResultIMC(props){  
+    return (
+        <View>
+            <Text>{props.messageResultImc}</Text>
+            <Text>{props.ResultImc}</Text>
+        </View>
+    );
+}
